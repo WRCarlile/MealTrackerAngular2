c02945b8be269a87ef3c7f3321baaebcecf84e9d
Ryan Carlile <ryancarlile@gmail.com>
2016-06-03 09:45:34 -0700

    App component displays model constructor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,29 @@
 import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>To-Do List</h1>
-      <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+      <h1>Meal Tracker</h1>
+      <h3 *ngFor="#meal of meals">{{meal.type }}:  {{ meal.description }}</h3>
     </div>
   `
 })
 export class AppComponent {
-  public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  public meals: Meal[];
+
   constructor(){
-    this.tasks = [
-      new Task("Create To-Do List app.", 0),
-      new Task("Learn Kung Fu.", 1),
-      new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3)
+    this.meals = [
+      new Meal("Hamburger", "Didn't get a soda or cheese on my burger!", 354),
+      new Meal("Fries", "I only ate half of them.", 365)
     ];
   }
 }
 
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {
-
-  }
-}
+// export class Task {
+//   public done: boolean = false;
+//   constructor(public description: string, public id: number) {
+//
+//   }
