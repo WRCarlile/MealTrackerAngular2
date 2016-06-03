a3a1285718d5a4bb8a17b8379e8fbafc6c03d1e9
Ryan Carlile <ryancarlile@gmail.com>
2016-06-03 09:49:47 -0700

    Add an edit component

---
+++ app/edit-album-details.component.ts
@@ -0,0 +1,18 @@
+import {Component} from 'angular2/core';
+import {Meal} from './meal.model';
+
+@Component({
+  selector: 'edit-meal-details',
+  inputs: ['meal'],
+  template: `
+    <div class="meal-form">
+      <h3>Edit Description: </h3>
+      <input [(ngModel)]="meal.type" class="input-lg meal-form"/>
+      <input [(ngModel)]="meal.desciption" class="input-lg meal-form"/>
+      <input [(ngModel)]="meal.calories" class="input-lg meal-form"/>
+    </div>
+  `
+})
+export class EditMealDetailsComponent {
+  public meal: Meal;
+}
